7dc5a6d129e7b0febb3219e2b41b8cd53c2a0389
Mchain <marbellachain@gmail.com>
2023-10-28 19:40:03 +0200

    Update index.vue

--- src/modules/[chain]/supply/index.vue
+++ src/modules/[chain]/supply/index.vue
@@ -8,6 +8,7 @@ import {
   type Coin,
 } from '@/types';
 import { onMounted } from 'vue';
+import { Icon } from '@iconify/vue';
 import PaginationBar from '@/components/PaginationBar.vue';
 const props = defineProps(['chain']);
 
@@ -53,6 +54,14 @@ function getFormattedDenom(denom: string) {
   return meta ? meta.display : denom;
 }
 
+function getIssuer(denom: string) {
+  return denom.split('-')[1];
+}
+
+function getDenomPretty(denom: string) {
+  return denom.split('-')[0];
+}
+
 function getFormattedAmount(denom: string, amount: string) {
   const meta = metadataList.value.find(
     (m) => m.base === denom || m.display === denom
@@ -73,29 +82,48 @@ function getFormattedAmount(denom: string, amount: string) {
         <thead>
           <tr>
             <td>Token</td>
-            <td>Description</td>
             <td class="text-right">Amount</td>
+            <td>Issuer</td>
+            <td></td>
           </tr>
         </thead>
         <template v-for="item in list" :key="item.denom">
-          <tr v-if="item.denom === 'umark' || item.denom === 'beer'">
-            <td class="uppercase">{{ getFormattedDenom(item.denom) }}</td>
-            <td>{{ getDescription(item.denom) }}</td>
+          <tr
+            v-if="item.denom === 'umark' || item.denom === 'beer'"
+            class="odd:bg-gray-100 dark:odd:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
+          >
+            <td class="uppercase">
+              {{ getFormattedDenom(item.denom) }}
+            </td>
             <td class="text-right">
               {{ getFormattedAmount(item.denom, item.amount) }}
-
-              <span class="ml-1 uppercase">{{
-                getFormattedDenom(item.denom)
-              }}</span>
             </td>
+            <td>Mchain</td>
+            <td></td>
           </tr>
         </template>
         <template v-for="item in list" :key="item.denom">
-          <tr v-if="item.denom != 'umark' && item.denom != 'beer'">
-            <td class="uppercase">{{ getFormattedDenom(item.denom) }}</td>
-            <td>{{ getDescription(item.denom) }}</td>
+          <tr
+            v-if="item.denom != 'umark' && item.denom != 'beer'"
+            class="odd:bg-gray-100 dark:odd:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
+          >
+            <td class="uppercase">
+              {{ getDenomPretty(getFormattedDenom(item.denom)) }}
+            </td>
             <td class="text-right">
               {{ getFormattedAmount(item.denom, item.amount) }}
+            </td>
+            <td>{{ getIssuer(item.denom) }}</td>
+            <td class="text-right">
+              <RouterLink
+                :to="`/${chain}/smarttoken/${item.denom}`"
+                class="btn btn-ghost btn-circle btn-sm mx-1"
+              >
+                <Icon
+                  icon="mdi:info-circle"
+                  class="text-2xl text-gray-500 dark:text-gray-400"
+                />
+              </RouterLink>
             </td>
           </tr>
         </template>

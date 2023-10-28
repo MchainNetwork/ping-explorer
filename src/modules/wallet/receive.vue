2ac91e7182753e7ea4ccb0e3d1e3c0b71f105efd
Mchain <marbellachain@gmail.com>
2023-10-28 11:56:15 +0200

    Update receive.vue

--- src/modules/wallet/receive.vue
+++ src/modules/wallet/receive.vue
@@ -3,16 +3,22 @@ import { useWalletStore } from '@/stores';
 import { useQRCode } from '@vueuse/integrations/useQRCode';
 
 const walletStore = useWalletStore();
-const qrcode = useQRCode(walletStore.currentAddress);
+const qrcode = useQRCode(walletStore.currentAddress, {
+  width: 200,
+  height: 200,
+});
 </script>
 
 <template>
   <div class="bg-base-100 p-4 rounded text-center">
-    <div class="text-xl font-semibold text-center">Pay Me</div>
-    <div v-if="walletStore.currentAddress" class="flex items-center justify-center mt-8 mb-4">
+    <h1 class="text-4xl font-bold mb-4 p-4">Your Wallet Address</h1>
+    <div
+      v-if="walletStore.currentAddress"
+      class="flex items-center justify-center mb-4"
+    >
       <img :src="qrcode" alt="QR Code" class="rounded-md overflow-hidden" />
     </div>
-    <div class="text-main">
+    <div class="text-main mb-4">
       {{ walletStore.currentAddress }}
     </div>
   </div>

473e5408c2ce6a909217ca987eab427c555405c5
Mchain <marbellachain@gmail.com>
2023-10-29 12:18:52 +0100

    calculator i18n

--- src/modules/[chain]/staking/calculator.vue
+++ src/modules/[chain]/staking/calculator.vue
@@ -59,6 +59,9 @@ const bondedTokensRatio = computed(() => {
 
 // APR Calculation
 const stakingAPR = computed(() => {
+  if (bondedTokensRatio.value === 0 || isNaN(bondedTokensRatio.value)) {
+    return 0;
+  }
   return (inflation.value * (1 - communityTax.value)) / bondedTokensRatio.value;
 });
 
@@ -101,64 +104,23 @@ const calculateReturns = (
 const returns = computed(() =>
   calculateReturns(stake.value, finalStakingAPR.value, compoundWeekly.value)
 );
-
-const cardData = ref([
-  {
-    title: 'Staking APR',
-    value: computed(() => `${stakingAPR.value.toFixed(2)}%`),
-  },
-  {
-    title: 'Community Tax',
-    value: computed(() => `${(communityTax.value * 100).toFixed(0)}%`),
-  },
-  {
-    title: 'Validator Commission',
-    value: computed(() => `${(validatorCommission.value * 100).toFixed(0)}%`),
-  },
-  {
-    title: 'Final Staking APR',
-    value: computed(() => `${finalStakingAPR.value.toFixed(2)}%`),
-  },
-  {
-    title: 'Token Price',
-    value: computed(() => `$${tokenValueUSD.value.toFixed(4)}`),
-  },
-
-  {
-    title: 'Bonded Tokens',
-    value: computed(
-      () => `${bondedTokens.value.toFixed(0)} ${bondDenomSymbol}`
-    ),
-  },
-  {
-    title: 'Actual Inflation',
-    value: computed(() => `${(inflation.value * 100).toFixed(2)}%`),
-  },
-  {
-    title: 'Tokens per Block',
-    value: computed(
-      () => `${tokensPerBlock.value.toFixed(2)} ${bondDenomSymbol}`
-    ),
-  },
-  {
-    title: 'Blocks per Year',
-    value: computed(() => `${blocksPerYear}`),
-  },
-  {
-    title: 'Total Supply',
-    value: computed(() => `${totalSupply.value.toFixed(0)} ${bondDenomSymbol}`),
-  },
-]);
 </script>
 
 <template>
-  <div class="mx-auto max-w-screen-md" v-if="finalStakingAPR">
+  <div class="mx-auto max-w-screen-md" v-if="stakingAPR > 0">
     <div class="bg-base-100 p-4 my-4 rounded-3xl text-center">
-      <h1 class="text-4xl font-bold mb-4 p-4">Staking Rewards Calculator</h1>
+      <h1 class="text-4xl font-bold mb-4 p-4">
+        {{ $t('calculator.staking_rewards_calculator') }}
+      </h1>
       <div class="mb-4">
-        <label for="stake" class="block text-md font-bold mb-4"
-          >Enter Your Mchain ({{ bondDenomSymbol }}) Stake</label
-        >
+        <label for="stake" class="block text-md font-bold mb-4">
+          {{
+            $t('calculator.enter_your_stake', {
+              chain: 'Mchain',
+              symbol: bondDenomSymbol,
+            })
+          }}
+        </label>
         <input
           type="number"
           id="stake"
@@ -168,24 +130,32 @@ const cardData = ref([
         />
       </div>
       <div class="flex items-center justify-between">
-        <div class="p-2">Your Investment = ${{ investment.toFixed(2) }}</div>
+        <div class="p-2">
+          {{ $t('calculator.your_investment') }} = ${{ investment.toFixed(2) }}
+        </div>
         <div class="form-control p-2">
           <label class="label cursor-pointer">
-            <span class="label-text mr-2">Compound Weekly</span>
+            <span class="label-text mr-2">{{
+              $t('calculator.compound_weekly')
+            }}</span>
             <input type="checkbox" class="checkbox" v-model="compoundWeekly" />
           </label>
         </div>
       </div>
       <div class="bg-base-300 px-4 py-8 rounded-3xl">
         <div class="mb-4">
-          <p class="text-sm font-medium text-gray-600">Staking APR</p>
+          <p class="text-sm font-medium text-gray-600">
+            {{ $t('calculator.staking_apr_title') }}
+          </p>
           <p class="text-xl font-bold text-green-600">
             {{ finalStakingAPR.toFixed(2) }}%
           </p>
         </div>
         <div class="grid sm:grid-cols-1 md:grid-cols-2 gap-4 mb-4">
           <div>
-            <p class="text-sm font-medium text-gray-600">Daily Returns</p>
+            <p class="text-sm font-medium text-gray-600">
+              {{ $t('calculator.daily_returns') }}
+            </p>
             <p class="text-xl font-bold">
               {{ returns.dailyTokens.toFixed(2) }} {{ bondDenomSymbol }}
             </p>
@@ -194,7 +164,9 @@ const cardData = ref([
             </p>
           </div>
           <div>
-            <p class="text-sm font-medium text-gray-600">Monthly Returns</p>
+            <p class="text-sm font-medium text-gray-600">
+              {{ $t('calculator.monthly_returns') }}
+            </p>
             <p class="text-xl font-bold">
               {{ returns.monthlyTokens.toFixed(2) }} {{ bondDenomSymbol }}
             </p>
@@ -203,7 +175,9 @@ const cardData = ref([
             </p>
           </div>
           <div class="md:col-span-2">
-            <p class="text-sm font-medium text-gray-600">Yearly Returns</p>
+            <p class="text-sm font-medium text-gray-600">
+              {{ $t('calculator.yearly_returns') }}
+            </p>
             <p class="text-xl font-bold">
               {{ returns.annualTokens.toFixed(2) }} {{ bondDenomSymbol }}
             </p>
@@ -213,51 +187,106 @@ const cardData = ref([
           </div>
         </div>
       </div>
+      <div class="mt-4 text-xs">
+        {{ $t('calculator.variable_apr_note') }}
+      </div>
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 my-4 text-center">
-      <div
-        class="bg-base-100 rounded-3xl"
-        v-for="(card, index) in cardData"
-        :key="index"
-      >
+      <div class="bg-base-100 rounded-3xl">
         <div class="p-4">
-          <p class="text-xs">{{ card.title }}</p>
-          <p class="text-md">{{ card.value }}</p>
+          <p class="text-xs">{{ $t('calculator.staking_apr_title') }}</p>
+          <p class="text-md">{{ Number(stakingAPR).toFixed(2) }}%</p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.community_tax_title') }}</p>
+          <p class="text-md">{{ (communityTax * 100).toFixed(0) }}%</p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">
+            {{ $t('calculator.validator_commission_title') }}
+          </p>
+          <p class="text-md">{{ (validatorCommission * 100).toFixed(0) }}%</p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.final_staking_apr_title') }}</p>
+          <p class="text-md">{{ Number(finalStakingAPR).toFixed(2) }}%</p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.token_price_title') }}</p>
+          <p class="text-md">$ {{ Number(tokenValueUSD).toFixed(4) }}</p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.bonded_tokens_title') }}</p>
+          <p class="text-md">
+            {{ Number(bondedTokens).toFixed(0) }} {{ bondDenomSymbol }}
+          </p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.actual_inflation_title') }}</p>
+          <p class="text-md">{{ (inflation * 100).toFixed(2) }}%</p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.tokens_per_block_title') }}</p>
+          <p class="text-md">
+            {{ Number(tokensPerBlock).toFixed(2) }} {{ bondDenomSymbol }}
+          </p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.blocks_per_year_title') }}</p>
+          <p class="text-md">{{ blocksPerYear }}</p>
+        </div>
+      </div>
+
+      <div class="bg-base-100 rounded-3xl">
+        <div class="p-4">
+          <p class="text-xs">{{ $t('calculator.total_supply_title') }}</p>
+          <p class="text-md">
+            {{ Number(totalSupply).toFixed(0) }} {{ bondDenomSymbol }}
+          </p>
         </div>
       </div>
     </div>
 
     <div class="bg-base-100 p-8 rounded-3xl">
-      <h2 class="text-lg font-semibold text-gray-700 mb-4">Disclaimer:</h2>
+      <h2 class="text-lg font-semibold text-gray-700 mb-4">
+        {{ $t('calculator.disclaimer_title') }}
+      </h2>
       <p class="text-sm text-gray-600">
-        The information provided by this Staking Rewards Calculator is for
-        general informational purposes only. All information on the site is
-        provided in good faith, however, we make no representation or warranty
-        of any kind, express or implied, regarding the accuracy, adequacy,
-        validity, reliability, availability, or completeness of any information
-        on the site.
+        {{ $t('calculator.disclaimer_text_1') }}
       </p>
       <p class="mt-4 text-sm text-gray-600">
-        The results generated by this calculator are based on the current
-        parameters of the blockchain and the input values provided by the user.
-        These may not reflect real-world conditions or future market behaviors.
-        The calculated rewards are estimates and should not be considered as
-        financial advice, investment guidance, or an encouragement to engage in
-        staking or any other financial activities.
+        {{ $t('calculator.disclaimer_text_2') }}
       </p>
       <p class="mt-4 text-sm text-gray-600">
-        Market conditions, token value fluctuations, staking policies, and other
-        factors can significantly affect the actual staking rewards. Users are
-        advised to conduct their own research and consult with financial experts
-        before making any investment decisions.
+        {{ $t('calculator.disclaimer_text_3') }}
       </p>
       <p class="mt-4 text-sm text-gray-600">
-        By using this calculator, you acknowledge that neither the developers
-        nor any associated parties will be responsible for any losses, damages,
-        or implications resulting from the use of, or reliance on, the
-        information provided by this calculator. The use of this calculator is
-        solely at your own risk.
+        {{ $t('calculator.disclaimer_text_4') }}
       </p>
     </div>
   </div>

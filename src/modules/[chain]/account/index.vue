d35873e98fc824368801720362037cd2268b6da0
Mchain <marbellachain@gmail.com>
2023-11-03 13:04:59 +0100

    Update index.vue

--- src/modules/[chain]/account/index.vue
+++ src/modules/[chain]/account/index.vue
@@ -3,75 +3,89 @@ import { computed, ref } from '@vue/reactivity';
 import { useBaseStore, useBlockchain, useFormatter } from '@/stores';
 import { PageRequest, type AuthAccount, type Pagination } from '@/types';
 import { onMounted } from 'vue';
+import IdentityIcon from '@/components/IdentityIcon.vue';
+
+// @ts-ignore
 import PaginationBar from '@/components/PaginationBar.vue';
 const props = defineProps(['chain']);
 
-const chainStore = useBlockchain()
+const chainStore = useBlockchain();
 
-const accounts = ref([] as AuthAccount[])
-const pageRequest = ref(new PageRequest())
-const pageResponse = ref({} as Pagination)
+const accounts = ref([] as AuthAccount[]);
+const pageRequest = ref(new PageRequest());
+const pageResponse = ref({} as Pagination);
 
 onMounted(() => {
-  pageload(1)
+  pageload(1);
 });
 
 function pageload(p: number) {
-  pageRequest.value.setPage(p)
-  chainStore.rpc.getAuthAccounts(pageRequest.value).then(x => {
-    accounts.value = x.accounts
-    pageResponse.value = x.pagination
+  pageRequest.value.setPage(p);
+  chainStore.rpc.getAuthAccounts(pageRequest.value).then((x) => {
+    accounts.value = x.accounts;
+    pageResponse.value = x.pagination;
   });
 }
 
 function showType(v: string) {
-    return v.replace("/cosmos.auth.v1beta1.", "")
+  return v.replace('/cosmos.auth.v1beta1.', '');
 }
 function findField(v: any, field: string) {
-    if(!v || Array.isArray(v) || typeof v === 'string') return null
-    const fields = Object.keys(v)
-    if(fields.includes(field)) {
-        return v[field]
-    }
-    for(let i= 0; i < fields.length; i++) {
-        const re: any = findField(v[fields[i]], field)
-        if(re) return re
-    }
+  if (!v || Array.isArray(v) || typeof v === 'string') return null;
+  const fields = Object.keys(v);
+  if (fields.includes(field)) {
+    return v[field];
+  }
+  for (let i = 0; i < fields.length; i++) {
+    const re: any = findField(v[fields[i]], field);
+    if (re) return re;
+  }
 }
 function showAddress(v: any) {
-    return findField(v, 'address')
+  return findField(v, 'address');
 }
 function showAccountNumber(v: any) {
-    return findField(v, 'account_number')
+  return findField(v, 'account_number');
 }
 function showSequence(v: any) {
-    return findField(v, 'sequence')
+  return findField(v, 'sequence');
 }
 function showPubkey(v: any) {
-    return findField(v, 'pub_key')
+  return findField(v, 'pub_key');
 }
-
 </script>
 <template>
-    <div class=" overflow-x-auto">
-        <table class="table table-compact">
-            <thead>
-                <tr>
-                    <td>{{ $t('account.type') }}</td>
-                    <td>{{ $t('account.address') }}</td>
-                    <td>{{ $t('account.acc_num') }}</td>
-                    <td>{{ $t('account.sequence') }}</td>
-                    <td>{{ $t('account.pub_key') }}</td>
-                </tr>
-            </thead>
-            <tr v-for="acc in accounts">
-                <td>{{ showType(acc['@type']) }}</td>
-                <td><RouterLink :to="`/${chain}/account/${showAddress(acc)}`">{{ showAddress(acc) }}</RouterLink></td>
-                <td>{{ showAccountNumber(acc) }}</td>
-                <td>{{ showSequence(acc) }}</td>
-                <td>{{ showPubkey(acc) }}</td>
-            </tr>
-        </table>
-        <PaginationBar :limit="pageRequest.limit" :total="pageResponse.total" :callback="pageload" />
+  <div class="overflow-auto mx-auto max-w-screen-lg">
+    <div class="flex justify-between items-center">
+      <h1 class="text-4xl font-bold mb-6 p-4">Accounts</h1>
+      <div class="pr-4"></div>
     </div>
+
+    <div class="bg-base-100 p-4 rounded-3xl">
+      <table class="table table-compact">
+        <thead>
+          <tr>
+            <td>{{ $t('account.address') }}</td>
+            <td width="50%">{{ $t('account.type') }}</td>
+          </tr>
+        </thead>
+        <tr :key="acc" v-for="acc in accounts">
+          <td class="flex items-center">
+            <IdentityIcon size="sm" :address="showAddress(acc)" class="mr-4" />
+            <RouterLink
+              :to="`/${chain}/account/${showAddress(acc)}`"
+              class="link link-primary hover:underline"
+              >{{ showAddress(acc) }}</RouterLink
+            >
+          </td>
+          <td>{{ showType(acc['@type']) }}</td>
+        </tr>
+      </table>
+      <PaginationBar
+        :limit="pageRequest.limit"
+        :total="pageResponse.total"
+        :callback="pageload"
+      />
+    </div>
+  </div>
 </template>
